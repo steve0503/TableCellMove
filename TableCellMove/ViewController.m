b42511c6d38eb6e85741f2beee7b2cc59dd53726
steve0503 <leehr76@gmail.com>
2014-01-07 20:54:17 +0900

    TableCellMove_3_4_13
    Bug

--- TableCellMove/ViewController.m
+++ TableCellMove/ViewController.m
@@ -8,16 +8,121 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define CELL_ID @"CELL_ID"
+
+@interface ViewController ()<UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITextField *userInput;
+
+@property (weak, nonatomic) IBOutlet UITableView *table;
+
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    
+    NSMutableArray *data;
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return [data count];
+    
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    
+    cell.textLabel.text = [data objectAtIndex:indexPath.row];
+    
+    return cell;
+    
+}
+
+//셀편집 승인 작업
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [data removeObjectAtIndex:indexPath.row];
+        
+        // 테이블에서 셀을 삭제해서 데이터와 동기화
+        
+        NSArray *rows = [NSArray arrayWithObject:indexPath];
+        [self.table deleteRowsAtIndexPaths:rows withRowAnimation:UITableViewRowAnimationAutomatic];
+        
+    }
+    
+}
+
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    return YES;
+}
+
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    
+    NSObject *obj = [data objectAtIndex:sourceIndexPath.row];
+    
+    [data removeObjectAtIndex:sourceIndexPath.row];
+    [data insertObject:obj atIndex:destinationIndexPath.row];
+    
+}
+
+// Editing Cell 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+-(IBAction)addItem:(id)sender{
+    
+    NSString *inputStr = self.userInput.text;
+    
+    if ([inputStr length] > 0) {
+        
+        
+    [data addObject:inputStr];
+        
+    
+    
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:([data count] -1) inSection:0];
+    
+    NSArray *row = [NSArray arrayWithObject:indexPath];
+    
+    [self.table insertRowsAtIndexPaths:row withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+    self.userInput.text = @"";
+    
+    }
+    
+    
+}
+
+-(IBAction)toggleEditMode:(id)sender{
+    
+    self.table.editing = !self.table.editing;
+    
+    ((UIBarButtonItem *)sender).title = self.table.editing?@"Done":@"Edit";
+    
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    
+    [textField resignFirstResponder];
+    
+    [self addItem:nil];
+    
+    return YES;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
